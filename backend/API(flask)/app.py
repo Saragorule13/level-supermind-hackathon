f615404146f48dfb9d2eb97a853b726a3aecd3c7
Prashant Dhuri <148627279+PrashantDhuri08@users.noreply.github.com>
2025-01-19 04:45:18 +0530

    port defined

--- backend/API(flask)/app.py
+++ backend/API(flask)/app.py
@@ -35,3 +35,8 @@ class Item(BaseModel):
 @app.post("/api/data")
 async def post_data(item: Item):
     return {"you_sent": item}
+
+
+if __name__ == '__main__': 
+    import uvicorn 
+    uvicorn.run(app, host="127.0.0.1", port=8000)
